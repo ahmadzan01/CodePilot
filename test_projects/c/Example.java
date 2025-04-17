a20d85106f7eda0997f0b2512780903896ce9b3e
Naqeeb <ahmadzan@mcmaster.ca>
2025-04-17 10:18:01 -0400

    another commit

---
+++ test_projects/c/Example.java
@@ -0,0 +1,13 @@
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+    }
+
+    private void helperMethod() {
+        // does something
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ test_projects/c/exmaple.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void greet() {
+    printf("Hello from C!\n");
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    greet();
+    return 0;
+}
